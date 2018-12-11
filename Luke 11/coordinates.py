14ff0e4c17935bd910e310ae99f1eb4ae8c7d988
mortefal <morten_falstad@hotmail.com>
2018-12-11 09:15:19 +0100

    Luke 11 ferdig

--- Luke 11/coordinates.py
+++ Luke 11/coordinates.py
@@ -1,25 +1,20 @@
 import urllib.request
 
+response = urllib.request.urlopen('https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-crisscross.txt').read()
 
-response = urllib.request.urlopen('https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-crisscross.txt')
-coordinates = []
-for line in response:
-	coordinates.append(line)
+tekstinput = response.decode("utf-8")
 
 def getCood(coordinates):
 	x = 0
 	y = 0
-
-	for i in range(0, len(coordinates)):
-		for j in range(0, len(coordinates[i]), 2):
-			if coordinates[i][j] == "V":
-				x -= int(coordinates[i][j-1])
-			elif coordinates[i][j] == "H":
-				x += int(coordinates[i][j-1])
-			elif coordinates[i][j] == "B":
-				y -= int(coordinates[i][j-1])
-			elif coordinates[i][j] == "F":
-				y += int(coordinates[i][j-1])
+	for i in range(1, len(coordinates), 2):
+		if coordinates[i] == "H":
+			x += int(coordinates[i-1])
+		elif coordinates[i] == "V":
+			x -= int(coordinates[i-1])
+		elif coordinates[i] == "F":
+			y += int(coordinates[i-1])
+		elif coordinates[i] == "B":
+			y -= int(coordinates[i-1])
 	return [x,y]
-
-print(getCood(coordinates))
+print(getCood(tekstinput))
